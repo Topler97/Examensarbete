467648a5555dbfe6441261dd4ef43a75cd8bb776
juliabengtsson2 <julia.bengtsson2@hotmail.com>
2025-03-10 10:33:50 +0100

    fixed error with connection..

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
 const app = express();
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://juliabengtsson2:hemligt123@examensarbete.mc7cr.mongodb.net/?retryWrites=true&w=majority&appName=Examensarbete')
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB', err);
+  });
+const port = process.env.PORT || 3000
 
-const port = 3000
-const url = 'localhost'
-
-app.listen(url, port, () => {
-    console.log(`listen to ${url}:${port}`)
+app.listen(port, () => {
+    console.log(`listen to ${port}`)
 })
