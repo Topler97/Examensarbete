5840ed6b8dbc46dd3d90525c1ff58354dd1c936b
Topler97 <yhjs23.toplam@folkuniversitetet.nu>
2025-03-13 10:37:15 +0100

    added css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-    content: [
-      './pages/**/*.{js,ts,jsx,tsx}',
-      './components/**/*.{js,ts,jsx,tsx}', // Lägg till om du har komponenter
-    ],
-    theme: {
-      extend: {},
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}", // Lägg till om du har komponenter
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [`poppins`, `inter`, `sans-serif`],
+      },
     },
-    plugins: [],
-}
+  },
+  plugins: [],
+};
