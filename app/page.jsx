4280742cdb6737346b9afa5cc79f166ea0f024ff
juliabengtsson2 <julia.bengtsson2@hotmail.com>
2025-03-14 14:00:02 +0100

    commit

--- app/page.jsx
+++ app/page.jsx
@@ -4,12 +4,6 @@ export default function Home() {
   return (
     <div>
       <h1>Välkommen till Next.js!</h1>
-      <div className="m-20">
-        <Button
-          buttonText={'Våra produkter'}
-          buttonColor={'#5B3636'}
-        />
-      </div>
     </div>
   );
 }
