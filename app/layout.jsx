daaed1e8f76100715c8b3f8474efb296006ee353
juliabengtsson2 <julia.bengtsson2@hotmail.com>
2025-03-17 16:19:01 +0100

    added hero and footer to layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import Navbar from "./components/navigation/Nav"; // Importera Navbar
+import Footer from './components/Footer';
 
 export const metadata = {
   title: 'Next.js',
@@ -12,6 +13,8 @@ export default function RootLayout({ children }) {
       <body>
         <Navbar /> 
         {children}
+        <Hero/>
+        <Footer/>
 
       </body>
     </html>
--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,8 @@
 
-import { Button } from "./components/Button"
-import { Hero } from "./components/Hero"
 
 export default function Home() {
   return (
   <div>
     <h1>Välkommen till Next.js!</h1>
-      <Hero/>
   </div>
 )}
