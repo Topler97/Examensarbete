2c249258159680c81ce3b480d71a38e8b5d49182
juliabengtsson2 <julia.bengtsson2@hotmail.com>
2025-03-19 19:49:05 +0100

    created intstructions component

--- app/components/Instructions.jsx
+++ app/components/Instructions.jsx
@@ -2,12 +2,18 @@
 
 export const Instructions = () => {
     return(
-        <section>
+        <section className="my-15 lg:mx-20">
             <div className="bg-primary text-white uppercase font-bold flex justify-center py-2 md:justify-start md:pl-5">
                 <h3>Instruktioner</h3>
             </div>
-            <div>
-                
+            <div className="mx-4 pt-5">
+                <ul>
+                    <li>Fyll en shaker med is.</li>
+                    <li>Tillsätt vodka, kaffelikör och färsk espresso.</li>
+                    <li>Skaka ordentligt i cirka 15-20 sekunder för att blanda ingredienserna och kyla drinken.</li>
+                    <li>Sila upp i ett kylt martiniglas.</li>
+                    <li>Dekorera med 2-3 kaffebönor på toppen.</li>
+                </ul>
             </div>
         </section>
     )
