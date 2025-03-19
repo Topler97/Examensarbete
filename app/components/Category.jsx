09911adf7b34bef15141574352f7b72fd7562771
juliabengtsson2 <julia.bengtsson2@hotmail.com>
2025-03-19 15:38:47 +0100

    created category component

---
+++ app/components/Category.jsx
@@ -0,0 +1,16 @@
+
+export const Category = () => {
+    return(
+        <section className="bg-[#F0F0F0] w-[80%] m-auto sm:w-[100%] sm:pb-5">
+            <div className="text-[24px] text-[#5B3636] font-semibold font-primary flex flex-col items-center sm:flex sm:flex-row sm:justify-around">
+                <h2 className="border-b-2 border-[#5B3636] sm:hover:border-b-2 sm:border-b-0  px-5 pt-4 cursor-pointer">Drinkar</h2>
+
+                <h2 className="border-b-2 border-[#5B3636] sm:hover:border-b-2 sm:border-b-0  px-5 pt-4 cursor-pointer">Vin</h2>
+                <h2 className="border-b-2 border-[#5B3636] sm:hover:border-b-2 sm:border-b-0  px-5 pt-4 cursor-pointer">Öl</h2>
+                <h2 className="border-b-2 border-[#5B3636] sm:hover:border-b-2 sm:border-b-0  px-5 pt-4 cursor-pointer">Cider</h2>
+                <h2 className="border-b-2 border-[#5B3636] sm:hover:border-b-2 sm:border-b-0  px-5 pt-4 cursor-pointer">Shots</h2>
+                <h2 className="pt-4 pb-10 sm:pb-0 cursor-pointer sm:hover:border-b-2">Alkoholfritt</h2>
+            </div>
+        </section>
+    )
+}
