21aef30097acdec434e3b0201a73b961e019447b
Topler97 <yhjs23.toplam@folkuniversitetet.nu>
2025-03-19 21:05:43 +0100

    page

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,8 @@
-import ImageGallery from "./components/supply/Supplay"
-import MyPineapple from "./components/allproduct/AllProduct"
+
 
 export default function Home() {
   return (
   <div>
-    <MyPineapple />
-    <ImageGallery />
+   
   </div>
 )}
