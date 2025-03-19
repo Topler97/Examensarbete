3fbbefa6a0eddc2fb9a655c619781f2c358e83f9
Topler97 <yhjs23.toplam@folkuniversitetet.nu>
2025-03-19 21:04:59 +0100

    Added image not beer anymore

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,10 @@
+import ImageGallery from "./components/supply/Supplay"
+import MyPineapple from "./components/allproduct/AllProduct"
 
 export default function Home() {
   return (
   <div>
-    
+    <MyPineapple />
+    <ImageGallery />
   </div>
 )}
